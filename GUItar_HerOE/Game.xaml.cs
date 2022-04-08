791fe13a61a3d71073a5b85030b24c842816d91c
Hegedus Bence <hegedus.bence@stud.uni-obuda.hu>
2022-04-08 16:37:44 +0200

    back buttons connected

--- GUItar_HerOE/Game.xaml.cs
+++ GUItar_HerOE/Game.xaml.cs
@@ -23,5 +23,10 @@ namespace GUItar_HerOE
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- GUItar_HerOE/Levels.xaml.cs
+++ GUItar_HerOE/Levels.xaml.cs
@@ -24,5 +24,10 @@ namespace GUItar_HerOE
             InitializeComponent();
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
