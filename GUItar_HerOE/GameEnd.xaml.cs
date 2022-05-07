4cc15e44ba4292c45610d0676476a8606629359a
Blitz428 <szegedi.tamas@stud.uni-obuda.hu>
2022-05-07 10:55:32 +0200

    Added win and lose effects for GameEnd, yet to be implemented

--- GUItar_HerOE/GameEnd.xaml.cs
+++ GUItar_HerOE/GameEnd.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUItar_HerOE.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,15 +20,32 @@ namespace GUItar_HerOE
     /// </summary>
     public partial class GameEnd : Window
     {
+        //private MusicLogic musicLogic;
+        //private MediaElement mediaElement;
         public GameEnd(int point, string msg)
         {
             InitializeComponent();
+            //musicLogic = new MusicLogic();
+            //mediaElement = new MediaElement();
             lb_point.Content = point;
             lb_msg.Content = msg;
+            //if (msg== "Nem a játék a nehéz, Te vagy béna!")
+            //{
+            //    mediaElement.Source = new Uri(musicLogic.musicPlayer.songFolderPath + "lose.wav");
+                
+            //}
+            //else
+            //{
+            //    mediaElement.Source = new Uri(musicLogic.musicPlayer.songFolderPath + "win.wav");
+            //}
+            
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
             this.Close();
         }
     }
